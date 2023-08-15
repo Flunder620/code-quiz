b258577eb360af8f2d94d236215a3894b9ffe43c
Flunder620 <jguzman12310@yahoo.com>
2023-08-15 14:56:24 -0500

    added timer function in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,23 @@
 var startButton = document.querySelector(".start-button");
 
-var timer;
-var timerCount;
+startButton.addEventListener("click", startGame);
 
-startButton.addEventListener("click",startGame);
+// function init(){
+
+// }
+
+function startGame(){
+
+}
+
+function timer(){
+    var seconds = 60;
+    var timer = setInterval (function(){
+        document.getElementsByClassName("timer").innerHTML = seconds;
+        sec--;
+        if(sec < 0) {
+            clearInterval(timer);
+        }
+    }, 1000)
+}
+function timer()
